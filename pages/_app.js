ba52d7a288643654cea3c1842734541fcfea9739
Rohit-del-source <rohitverma18158920@gmail.com>
2024-01-10 12:09:01 +0530

    first

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,24 @@
 import React from 'react';
 
 import MyComponent from '../components/MyComponent';
+import { ThemeProvider } from '@/contexts/ThemeContext';
+import { UserProvider } from '@/contexts/UserContext';
+import { LanguageProvider } from '@/contexts/LanguageContext';
+import { ConfigProvider } from '@/contexts/ConfigContext';
 
 function App() {
   return (
-            <MyComponent />
+
+        <ThemeProvider>
+          <UserProvider>
+            <LanguageProvider>
+              <ConfigProvider>
+                <MyComponent />
+              </ConfigProvider>
+            </LanguageProvider>
+          </UserProvider>
+        </ThemeProvider>
+            
   );
 }
 
